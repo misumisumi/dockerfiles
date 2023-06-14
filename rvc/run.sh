5a2fe16131377424442b60689769b2171a3375b0
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-14 15:57:22 +0900

    update: pretrained model path

--- rvc/run.sh
+++ rvc/run.sh
@@ -4,7 +4,7 @@ podman run --runtime nvidia -it \
   -p 7865:7865 \
   -v /run/user/1000/pulse/pulpul:/tmp/pulse/native \
   -v "$(pwd)/Retrieval-based-Voice-Conversion-WebUI":/app/rvc \
-  -v "$(pwd)/rvc_weights":/app/weights \
+  -v "$(pwd)/pretrained/rvc":/app/weights \
   -v "$(pwd)/../results/rvc":/app/results \
   -v "$(pwd)/../datasets/48k":/app/datasets/48k \
   -v "$(pwd)/dockerfiles/rvc/scripts":/app/scripts \
