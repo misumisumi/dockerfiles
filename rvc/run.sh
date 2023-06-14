7d8cdf6662ba8908923caa0fb291c42ed1870bd5
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-14 15:26:50 +0900

    update: rvcの起動スクリプトの変更

--- rvc/run.sh
+++ rvc/run.sh
@@ -5,7 +5,9 @@ podman run --runtime nvidia -it \
   -v /run/user/1000/pulse/pulpul:/tmp/pulse/native \
   -v "$(pwd)/Retrieval-based-Voice-Conversion-WebUI":/app/rvc \
   -v "$(pwd)/rvc_weights":/app/weights \
+  -v "$(pwd)/../results/rvc":/app/results \
   -v "$(pwd)/../datasets/48k":/app/datasets/48k \
+  -v "$(pwd)/dockerfiles/rvc/scripts":/app/scripts \
   -w /app/rvc \
   --shm-size=4g \
   --name rvc \
