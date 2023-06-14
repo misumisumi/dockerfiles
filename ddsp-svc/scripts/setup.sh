bd2626a1eeab96446ae637c7ebae3654dc899813
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-14 16:08:08 +0900

    update: pretrained path

--- ddsp-svc/scripts/setup.sh
+++ ddsp-svc/scripts/setup.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-ln -sf "$(pwd)"/../pretrained/ddsp-svc/pretrain/hubert/* ./pretrain/hubert
-ln -sf "$(pwd)"/../pretrained/ddsp-svc/pretrain/nfs_hifigan/* ./pretrain/nfs_hifigan
-ln -sf "$(pwd)"/../pretrained/ddsp-svc/pretrain/checkpoint_best_legacy_500.pt ./pretrain/
+ln -sf "$(pwd)"/../pretrained/hubert/* ./pretrain/hubert
+ln -sf "$(pwd)"/../pretrained/nfs_hifigan/* ./pretrain/nfs_hifigan
+ln -sf "$(pwd)"/../pretrained/contentvec/* ./pretrain/contentvec
